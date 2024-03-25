91fc3e5ba87be24564348bb27df79add31d04460
Lazerneek123 <Lazerneek123@yandex.ru>
2024-03-25 23:17:19 +0200

    Added a new group Database Room and subdivisions to it (Create Table and Create DAO).

--- src/main/kotlin/com/example/demo/Main.kt
+++ src/main/kotlin/com/example/demo/Main.kt
@@ -7,7 +7,7 @@ import com.intellij.openapi.project.Project
 
 class Main : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        event.project?.showNotification("Hello World")
+        event.project?.showNotification("Plugin installed successfully!")
     }
 
     private fun Project.showNotification(message: String) {
---
+++ src/main/kotlin/com/example/demo/action/CreateDAO.kt
@@ -0,0 +1,10 @@
+package com.example.demo.action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class CreateDAO : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+
+    }
+}
